2bf93031590dfd83bbe541425856b4ac39f6a01b
rojanDinc <rojand94@gmail.com>
2025-02-01 17:56:26 +0100

    add plugin

---
+++ internal/plugin/plugin_test.go
@@ -0,0 +1,77 @@
+package plugin
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: RoundTripFunc(fn),
+	}
+}
+
+func TestPlugin(t *testing.T) {
+	cases := []struct {
+		name          string
+		settings      *Settings
+		resp          *http.Response
+		expectedError error
+	}{
+		{
+			name: "happy path",
+			settings: &Settings{
+				GithubToken:       "token",
+				Repository:        "repo",
+				Comment:           "comment",
+				PullRequestNumber: 1,
+			},
+			resp: &http.Response{
+				StatusCode: 201,
+				Body:       io.NopCloser(bytes.NewBufferString("")),
+				Header:     make(http.Header),
+			},
+			expectedError: nil,
+		},
+		{
+			name: "failed to create comment",
+			settings: &Settings{
+				GithubToken:       "token",
+				Repository:        "repo",
+				Comment:           "comment",
+				PullRequestNumber: 1,
+			},
+			resp: &http.Response{
+				StatusCode: 403,
+				Body:       io.NopCloser(bytes.NewBufferString(``)),
+				Header:     make(http.Header),
+			},
+			expectedError: errors.New("failed to create comment got unexpected status code"),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			client := NewTestClient(func(req *http.Request) *http.Response {
+				return tc.resp
+			})
+			p := NewPlugin("", client, tc.settings)
+
+			err := p.Execute(context.Background())
+
+			assert.Equal(t, err, tc.expectedError)
+		})
+	}
+}
---
+++ internal/plugin/settings.go
@@ -0,0 +1,31 @@
+package plugin
+
+import "errors"
+
+type Settings struct {
+	GithubToken       string
+	Repository        string
+	Comment           string
+	PullRequestNumber int64
+	Owner             string
+}
+
+func (s *Settings) Validate() error {
+	if s.GithubToken == "" {
+		return errors.New("GitHub token is required")
+	}
+
+	if s.Repository == "" {
+		return errors.New("GitHub repository is required")
+	}
+
+	if s.PullRequestNumber == 0 {
+		return errors.New("pull request number is required")
+	}
+
+	if s.Comment == "" {
+		return errors.New("comment is required")
+	}
+
+	return nil
+}
