2b1a686db209ea21e8bfc84e908203cf5e0e816f
rojanDinc <rojand94@gmail.com>
2025-04-09 00:08:21 +0200

    add required bool for required flags

--- internal/command/create.go
+++ internal/command/create.go
@@ -38,32 +38,44 @@ func (c *Create) flags() []cli.Flag {
 		&cli.StringFlag{
 			Name:        "token",
 			Usage:       "GitHub bearer token to be used for authentication",
+			Required:    true,
 			Sources:     cli.EnvVars("PLUGIN_GITHUB_TOKEN"),
 			Destination: &c.settings.GithubToken,
 		},
 		&cli.StringFlag{
 			Name:        "repo",
 			Usage:       "GitHub repository",
+			Required:    true,
 			Sources:     cli.EnvVars("CI_REPO"),
 			Destination: &c.settings.Repository,
 		},
 		&cli.IntFlag{
 			Name:        "pr-number",
 			Usage:       "Pull request number",
+			Required:    true,
 			Sources:     cli.EnvVars("CI_COMMIT_PULL_REQUEST"),
 			Destination: &c.settings.PullRequestNumber,
 		},
 		&cli.StringFlag{
 			Name:        "comment",
 			Usage:       "Comment to be added to pull request",
+			Required:    true,
 			Sources:     cli.EnvVars("PLUGIN_COMMENT"),
 			Destination: &c.settings.Comment,
 		},
 		&cli.StringFlag{
 			Name:        "owner",
 			Usage:       "Owner of the repository",
+			Required:    true,
 			Sources:     cli.EnvVars("CI_REPO_OWNER"),
 			Destination: &c.settings.Owner,
+		},
+		&cli.StringFlag{
+			Name:        "log-level",
+			Usage:       "Log level",
+			Sources:     cli.EnvVars("PLUGIN_LOG_LEVEL"),
+			Destination: &c.settings.LogLevel,
+			Value:       "info",
 		},
 	}
 }
